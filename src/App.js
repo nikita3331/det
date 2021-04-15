973ebd179604d19c9a9185d60c2c71c0e7a02525
Mykyta Brazhynskyy <nikita3331@gmail.com>
2021-04-15 19:36:38 +0200

    added web socket

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {StyleSheet,Text,TouchableOpacity,SafeAreaView} from 'react-native';
 
 import { createStore, applyMiddleware } from 'redux'
@@ -6,10 +6,21 @@ import reducer from './reducers'
 import thunkMiddleware from 'redux-thunk'
 import { Provider, useSelector } from 'react-redux'
 import Navigator from './navigator'
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+const client = new W3CWebSocket('ws://127.0.0.1:8010');
+
 const middleware = applyMiddleware(thunkMiddleware)
 const store = createStore(reducer, middleware) 
 
 const App=() => {
+  useEffect(()=>{
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message.data);
+    };
+  },[])
   return (
     <Provider store={store}>
       <SafeAreaView style={{flex:1}} edges={['right', 'top', 'left']}>
